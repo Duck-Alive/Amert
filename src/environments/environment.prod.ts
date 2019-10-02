7f0e78af9e6ae63f88dad83a6be739536cc606ec
xovivo <36909654+xovivo@users.noreply.github.com>
2019-10-02 18:55:14 -0500

    v0018

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDpR_yxp-P71vhs3K5MaMlGmZQhTcBhCHc',
+    authDomain: 'chofix-450a5.firebaseapp.com',
+    databaseURL: 'https://chofix-450a5.firebaseio.com',
+    projectId: 'chofix-450a5',
+    storageBucket: 'chofix-450a5.appspot.com',
+    messagingSenderId: '29140314831',
+    appId: '1:29140314831:web:32050d5c11c955b4f425ba'
+  }
 };
